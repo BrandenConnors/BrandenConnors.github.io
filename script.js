9402e74e4a4995418c568e581b7a71276d3a987d
Branden Connors <86928471+BrandenConnors@users.noreply.github.com>
2022-03-15 12:13:06 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -26,14 +26,14 @@ $('.scroll-up-btn').click(function(){
 
 // typing animation script
 var typed = new Typed(".typing", {
-    strings:["Full Stack Developer", "Designer", "Blogger", "Maker"],
+    strings:["Full Stack Developer", "Designer", "Software Engineer", "Maker"],
     typeSpeed: 100,
     backSpeed:60,
     loop: true,
 });
 
 var typed = new Typed(".typing-2", {
-    strings:["Full Stack Developer", "Designer", "Blogger", "Maker"],
+    strings:["Full Stack Developer", "Designer", "Software Engineer", "Maker"],
     typeSpeed: 100,
     backSpeed:60,
     loop: true,
